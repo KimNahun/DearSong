5d19203def120b9975f1ed44c69bcb63b6e14757
kimnahun <rlaskgns0212@naver.com>
2026-04-23 23:20:24 +0900

    fix: build-fix.md evaluate.md feedback.md

---
+++ DearSong/DearSong/DearSongApp.swift
@@ -0,0 +1,17 @@
+//
+//  DearSongApp.swift
+//  DearSong
+//
+//  Created by kimnahun on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct DearSongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
